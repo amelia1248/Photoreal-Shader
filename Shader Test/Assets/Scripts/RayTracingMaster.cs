aac7c8c95c84de6cee81129c34960164f8538066
Amelia Wu <ameliawu26@gmail.com>
2024-07-28 00:46:45 -0400

    Anti-Aliasing Finished

--- Shader Test/Assets/Scripts/RayTracingMaster.cs
+++ Shader Test/Assets/Scripts/RayTracingMaster.cs
@@ -5,6 +5,9 @@ public class RayTracingMaster : MonoBehaviour
     public ComputeShader RayTracingShader;  // Create Shader object in Unity
     private RenderTexture _target;  // Allows Shader to be assigned to GameObjects? A texture that can be rendered to.
     private Camera _camera; // Generate some camera rays
+    public Texture SkyboxTexture;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
 
 
     private void Awake(){
@@ -15,6 +18,8 @@ public class RayTracingMaster : MonoBehaviour
         // In our shader, we define matrices, Ray structure, and a function 
         RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);    // Setting Skybox Texture
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
     }
     
     /// <summary>
@@ -28,12 +33,26 @@ public class RayTracingMaster : MonoBehaviour
                 _target.Release();  // If _target exists, we release without being collected for garbage
             }
             // Initialize new target for ray tracing
+            // But first, reset currentSamples
+            _currentSample = 0;  // Note: fix?
             _target = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             _target.enableRandomWrite = true;   // Enables random access write into random texture
             _target.Create();   // Actually creates the RenderTexture
         }
     }
     
+
+    // Update function detects camera transform changes
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+
 
     /// <summary>
     /// Write to Unity's raw interface, executes Compute Shader to the screen
@@ -48,7 +67,12 @@ public class RayTracingMaster : MonoBehaviour
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);  // Executes our Compute Shader to the screen
 
-        Graphics.Blit(_target, destination);    // Raw interface to Unity's drawing function... blit copipes pixel data from a texture to a render texture
+        // Added code for PROGRESSIVE SAMPLING
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);    // Raw interface to Unity's drawing function... blit copipes pixel data from a texture to a render texture
+        _currentSample++;
     }
 
 
